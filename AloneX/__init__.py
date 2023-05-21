45f1b6259e2b1070369c3ef303fd96e02f045932
AloneXBot <132041403+AloneXBot@users.noreply.github.com>
2023-05-22 03:47:47 +0530

    Add files via upload

---
+++ AloneX/__init__.py
@@ -0,0 +1,41 @@
+from AloneX.core.bot import AloneXBot
+from AloneX.core.dir import dirr
+from AloneX.core.git import git
+from AloneX.core.userbot import Userbot
+from AloneX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = AloneXBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
